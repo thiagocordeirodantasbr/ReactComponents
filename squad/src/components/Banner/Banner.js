a193626fe9d8942bdb6e7919f72f5ad8fb79483a
Thiago Cordeiro <thiagocordeirodantasbr@gmail.com>
2023-07-30 02:19:42 -0300

    Criando o primeiro componente e estilizando

---
+++ squad/src/components/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+function Banner(){
+    return (
+        <header className="banner">
+            <img src="/img/banner.jpg" alt="banner da pagina principal"/>
+        </header>
+    )
+}
+
+
+export default Banner;
