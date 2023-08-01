75466ed2a035c58ca75ee61126e91257646e7840
Thiago Cordeiro <thiagocordeirodantasbr@gmail.com>
2023-08-01 01:00:40 -0300

    Cadastrando os agentes

--- squad/src/App.js
+++ squad/src/App.js
@@ -1,11 +1,19 @@
+import { useState } from 'react';
 import Banner from './components/Banner';
 import Formulario from './components/Formulario';
 
 function App() {
+
+  const [agentes, setAgentes] = useState([])
+
+  const aoNovoAgenteAdicionado = (agente) =>{
+      console.log(agente)
+  }
+
   return (
     <div className="App">
       <Banner/>
-      <Formulario/>
+      <Formulario aoAgenteCadastrado={agente => aoNovoAgenteAdicionado(agente)}/>
     </div>
   );
 }
--- squad/src/components/Formulario/index.js
+++ squad/src/components/Formulario/index.js
@@ -1,9 +1,10 @@
+import { useState } from 'react';
 import Botao from '../Botao';
 import CampoTexto from '../CampoTexto';
 import ListaSuspensa from '../ListaSuspensa';
 import './Formulario.css'
 
-const Formulario = () => {
+const Formulario = (props) => {
 
     const classes = [
         'Controladores',
@@ -12,23 +13,32 @@ const Formulario = () => {
         'Sentinelas'
     ]
 
+    const [nome, setNome] = useState('');
+    const [cargo, setCargo] = useState('');
+    const [imagem, setImagem] = useState('');
+    const [classe, setClasse] = useState('');
+
+
     const aoSalvar = (event) => {
-        console.log('oi');
         event.preventDefault()
-        
+        props.aoAgenteCadastrado({
+            nome: nome,
+            cargo: cargo,
+            imagem: imagem,
+            classe: classe
+        })
+       
     }
 
     return (   
         <section className="formulario">
             <form onSubmit={aoSalvar} >
                 <h2>Preencha os dados para criar o card do valorant</h2>
-                <CampoTexto  obrigatorio={true} label="Nome" placeholder="Digite o seu nome" />
-                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite o seu Cargo" />
-                <CampoTexto  obrigatorio={true} label="Imagem" placeholder="Digite a URL da imagem" />
-                <ListaSuspensa obrigatorio={true} label="Classes" itens={classes} />
-                <Botao>
-                    Criar Card
-                </Botao>
+                <CampoTexto  obrigatorio={true} label="Nome"placeholder="Digite o seu nome" valor={nome} aoAlterado={valor => setNome(valor)} />
+                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite o seu Cargo" valor={cargo} aoAlterado={valor => setCargo(valor)} />
+                <CampoTexto  obrigatorio={true} label="Imagem" placeholder="Digite a URL da imagem" valor={imagem} aoAlterado={valor => setImagem(valor)} />
+                <ListaSuspensa obrigatorio={true} label="Classes" itens={classes}  valor={classe} aoAlterado={valor => setClasse(valor)} />
+                <Botao>Criar Card</Botao>
             </form>
         </section>
     )
