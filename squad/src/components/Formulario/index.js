ccf90c768c3b6933b1f5053d08835d80dbe356a2
Thiago Cordeiro <thiagocordeirodantasbr@gmail.com>
2023-07-31 21:37:36 -0300

    Criando o formulario

--- squad/src/components/Formulario/index.js
+++ squad/src/components/Formulario/index.js
@@ -1,7 +1,17 @@
+import Botao from '../Botao';
 import CampoTexto from '../CampoTexto';
+import ListaSuspensa from '../ListaSuspensa';
 import './Formulario.css'
 
 const Formulario = () => {
+
+    const classes = [
+        'Controladores',
+        'Duelistas',
+        'Iniciadores',
+        'Sentinelas'
+    ]
+
     return (   
         <section className="formulario">
             <form>
@@ -9,6 +19,10 @@ const Formulario = () => {
                 <CampoTexto label="Nome" placeholder="Digite o seu nome" />
                 <CampoTexto label="Cargo" placeholder="Digite o seu Cargo" />
                 <CampoTexto label="Imagem" placeholder="Digite a URL da imagem" />
+                <ListaSuspensa  label="Classes" itens={classes} />
+                <Botao>
+                    Criar Card
+                </Botao>
             </form>
         </section>
     )
