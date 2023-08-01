cf604cd422904f87aa53ebad98ed75b6b1f24247
Thiago Cordeiro <thiagocordeirodantasbr@gmail.com>
2023-07-31 22:20:22 -0300

    Escutando eventos

--- squad/src/components/Formulario/index.js
+++ squad/src/components/Formulario/index.js
@@ -12,14 +12,20 @@ const Formulario = () => {
         'Sentinelas'
     ]
 
+    const aoSalvar = (event) => {
+        console.log('oi');
+        event.preventDefault()
+        
+    }
+
     return (   
         <section className="formulario">
-            <form>
+            <form onSubmit={aoSalvar} >
                 <h2>Preencha os dados para criar o card do valorant</h2>
-                <CampoTexto label="Nome" placeholder="Digite o seu nome" />
-                <CampoTexto label="Cargo" placeholder="Digite o seu Cargo" />
-                <CampoTexto label="Imagem" placeholder="Digite a URL da imagem" />
-                <ListaSuspensa  label="Classes" itens={classes} />
+                <CampoTexto  obrigatorio={true} label="Nome" placeholder="Digite o seu nome" />
+                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite o seu Cargo" />
+                <CampoTexto  obrigatorio={true} label="Imagem" placeholder="Digite a URL da imagem" />
+                <ListaSuspensa obrigatorio={true} label="Classes" itens={classes} />
                 <Botao>
                     Criar Card
                 </Botao>
