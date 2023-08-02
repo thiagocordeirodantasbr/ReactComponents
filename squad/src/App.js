d7c1bb04db480bba01177c994c814ed33ecb8dec
Thiago Cordeiro <thiagocordeirodantasbr@gmail.com>
2023-08-02 14:47:10 -0300

    criando o componente time

--- squad/src/App.js
+++ squad/src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Banner from './components/Banner';
 import Formulario from './components/Formulario';
+import Time from './components/Time';
 
 function App() {
 
@@ -8,12 +9,17 @@ function App() {
 
   const aoNovoAgenteAdicionado = (agente) =>{
       console.log(agente)
+      setAgentes(...agentes, agente)
   }
 
   return (
     <div className="App">
       <Banner/>
       <Formulario aoAgenteCadastrado={agente => aoNovoAgenteAdicionado(agente)}/>
+      <Time nome="Controladores"/>
+      <Time nome="Duelistas"/>
+      <Time nome="Iniciadores"/>
+      <Time nome="Sentinelas"/>
     </div>
   );
 }
