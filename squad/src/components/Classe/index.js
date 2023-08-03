e62ec5e5c7ed70388e38c3f9e9d71aca3923d419
Thiago Cordeiro <thiagocordeirodantasbr@gmail.com>
2023-08-02 23:58:47 -0300

    finalizando os times

--- squad/src/components/Classe/index.js
+++ squad/src/components/Classe/index.js
@@ -3,13 +3,14 @@ import './Classes.css';
 
 const Classes = (props) => {
     return (
-    <section className="classe" style={ { backgroundColor: props.corSecundaria }}>
+    (props.agentes.length > 0) ? <section className="classe" style={ { backgroundColor: props.corSecundaria }}>
             <h3 style={{ borderColor: props.corPrimaria}}>{props.nome}</h3>
             <div className="agentes">
                 {props.agentes.map( agente =>  <Agente nome={agente.nome} cargo={agente.cargo} imagem={agente.imagem} />)}
             </div>
           
         </section>
+        : ''
     )
 }
 
